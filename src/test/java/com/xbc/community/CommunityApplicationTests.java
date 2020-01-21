3e4ceaf4a9e5e948974acc31fa46bd4a33fafbb8
yyxbc <aaa376968779@qq.com>
2020-01-21 19:42:11 +0800

    init repo

---
+++ src/test/java/com/xbc/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xbc.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
